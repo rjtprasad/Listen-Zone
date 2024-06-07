b7e7ece202304df39006be1c4b12761948deef2d
rjtprasad <prasadrajat28@gmail.com>
2024-06-07 11:02:55 +0530

    Newsletter UI

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import bannerImg from '../../../assets/bannerImg.png'
 import './Banner.scss'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='hero-banner'>
+      <div className='content'>
+        <div className='text-content'>
+          <h1>
+            Latest Headphones
+          </h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus, ad.</p>
+          <div className='ctas'>
+            <button className='banner-cta'>Buy Now</button>
+          </div>
+        </div>
+        
+        <img className='banner-img' src={bannerImg} alt='banner'/>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Banner from './Banner/Banner'
+
 
 import './Home.scss'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Banner/>
+    </div>
   )
 }
 
