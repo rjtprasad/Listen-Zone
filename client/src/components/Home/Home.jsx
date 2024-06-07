ed4e13dc805e28e218c943ed33d8949f7b597b0c
rjtprasad <prasadrajat28@gmail.com>
2024-06-07 13:04:00 +0530

    Category Section UI

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import Banner from './Banner/Banner'
+import React from "react";
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
 
-
-import './Home.scss'
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Banner/>
+    <div>
+      <Banner />
+      <div className="main-content">
+        <div className="layout">
+          <Category />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
