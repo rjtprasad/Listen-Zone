34c3e6743fe873c3ec284dc37a01279e67608e22
rjtprasad <prasadrajat28@gmail.com>
2024-06-09 12:16:11 +0530

    Search UI

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,16 +4,19 @@ import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
 import Cart from "../Cart/Cart";
+import Search from "../Search/Search";
 
 import "./Header.scss";
 
 const Header = () => {
   const [scroll, setScroll] = useState(false);
-  const [showCart, setShowCart] = useState(false)
+  const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
   const navigate = useNavigate();
 
   const handleScroll = () => {
-    const offset = window.scrollY
+    const offset = window.scrollY;
     if (offset > 200) {
       setScroll(true);
     } else {
@@ -27,28 +30,28 @@ const Header = () => {
 
   return (
     <>
-    
-    <header className={`main-header ${scroll ? 'sticky-header' : ''}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div onClick={() => navigate("/")} className="center">
-          Listen Zone
-        </div>
-        <div className="right">
-          <TbSearch />
-          <AiOutlineHeart />
-          <div className="cart-icon" onClick={()=>setShowCart(true)}>
-            <CgShoppingCart />
-            <span>5</span>
+      <header className={`main-header ${scroll ? "sticky-header" : ""}`}>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div onClick={() => navigate("/")} className="center">
+            Listen Zone
+          </div>
+          <div className="right">
+            <TbSearch onClick={()=>setShowSearch(true)}/>
+            <AiOutlineHeart />
+            <div className="cart-icon" onClick={() => setShowCart(true)}>
+              <CgShoppingCart />
+              <span>5</span>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
-    {showCart && <Cart setShowCart={setShowCart}/>}
+      </header>
+      {showCart && <Cart setShowCart={setShowCart} />}
+      {showSearch && <Search setShowSearch={setShowSearch}/>}
     </>
   );
 };
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { MdClose } from "react-icons/md";
+import earbud from "../../assets/products/earbuds-prod-1.webp";
 
-import './Search.scss'
+import "./Search.scss";
 
-const Search = () => {
+const Search = ({ setShowSearch }) => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search-function">
+      <div className="form-field">
+        <input type="text" autoFocus placeholder="Search for products" />
+        <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+      <div className="search-result-content">
+        <div className="search-results">
+          <div className="search-result-item">
+            <div className="image-container">
+              <img src={earbud} alt="earbud" />
+            </div>
+            <div className="prod-details">
+              <span className="name">product name</span>
+              <span className="desc">product desc</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
